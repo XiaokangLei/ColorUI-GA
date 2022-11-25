47e22a8a1e5775c79fd900c2e333825d6880167f
Xiaokang Lei <13281183@bjtu.edu.cn>
2022-11-25 19:19:12 +0800

    Revert "remove unused code"
    This reverts commit 637545f7e4b468a56207741857bd364854a72ab1.

--- pages/about/home/home.js
+++ pages/about/home/home.js
@@ -90,7 +90,7 @@ Component({
       } else {
         that.setData({
           starCount: that.coutNum(30),
-          forksCount: that.coutNum(582),
+          forksCount: that.coutNum(532),
           visitTotal: that.coutNum(24)
         })
       }
